3612af77db6536349455c843f472611e3ecebca5
Silver Sepp <sepp.silver@outlook.com>
2018-03-06 01:59:42 +0200

    Remove frontend project files

--- frontend/task-manager/src/task.js
+++
@@ -1,6 +0,0 @@
-export class Task {
-    constructor(description) {
-        this.description = description;
-        this.completed = false;
-    }
-}
--- frontend/task-manager/src/todo.js
+++
@@ -1,9 +0,0 @@
-export class Todo{
-	constructor(description){
-		this.description = description;
-		this.done = false;
-
-		this.priority = -1;
-		this.date = -1;
-	}
-}
