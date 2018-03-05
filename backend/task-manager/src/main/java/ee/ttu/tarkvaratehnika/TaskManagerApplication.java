106be78683623d859ab121566f56685e3d035a1c
Silver Sepp <sepp.silver@outlook.com>
2018-03-05 19:55:01 +0200

    Move TaskManagerApplication.java up one package

--- backend/task-manager/src/main/java/ee/ttu/tarkvaratehnika/TaskManagerApplication.java
+++ backend/task-manager/src/main/java/ee/ttu/tarkvaratehnika/TaskManagerApplication.java
@@ -1,4 +1,4 @@
-package ee.ttu.tarkvaratehnika.taskmanager;
+package ee.ttu.tarkvaratehnika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
