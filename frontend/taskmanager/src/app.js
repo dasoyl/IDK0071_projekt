e33e101709043a25bf97b47740d02edd6d16e462
Silver Sepp <sepp.silver@outlook.com>
2018-03-06 04:48:59 +0200

    Refactor navigation

--- frontend/taskmanager/src/app.js
+++ frontend/taskmanager/src/app.js
@@ -2,12 +2,12 @@ export class App {
   constructor() {
       this.userName = "TEST_USER";
   }
-  
+
   configureRouter(config, router){
         config.title = 'Tasks';
         config.map([
-          { route: '',              moduleId: 'home',   title: 'Home'},
-          { route: 'tasks/',  moduleId: 'tasks', title: 'Tasks' },
+          { route: '',              moduleId: 'home',   title: 'Home', nav:true},
+          { route: 'tasks/',  moduleId: 'tasks', title: 'Tasks', nav:true},
           { route: 'tasks/:id', moduleId: 'task-detail', title: 'Task'},
           { route: 'tasks/add', moduleId: 'add-task', title: 'Add Task'}
         ]);
